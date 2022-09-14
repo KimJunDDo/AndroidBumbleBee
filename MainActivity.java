23588a7092ae974ae64359757a94a2f2facb5292
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-09-14 17:08:01 +0900

    낫노

--- MainActivity.java
+++ MainActivity.java
@@ -1,61 +1,111 @@
-package com.cookandroid.hello;
+package com.cookandroid.natno
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnNate, btn911, btnGal, btnEnd;
+        EditText edit1, edit2;
+        Button btnAdd, btnSub, btnMul, btnDiv, btnRest;
+        TextView textResult;
+        String num1, num2;
+        Integer result;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @override
+        public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
 
-        btnNate = findViewById(R.id.btnNate);
-        btn911 = findViewById(R.id.btn911);
-        btnGal = findViewById(R.id.btnGal);
-        btnEnd = findViewById(R.id.btnEnd);
+        edit1 = (EditText)findViewById(R.id.Edit1);
+        edit2 = (EditText)findViewById(R.id.Edit2);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnSub = (Button)findViewById(R.id.btnSub);
+        btnMul = (Button)findViewById(R.id.btnMul);
+        btnDiv = (Button)findViewById(R.id.btnDiv);
+        btnRest = (Button)findViewById(R.id.btnRest);
+        textResult = (TextView)findViewById(R.id.textResult);
 
-        btnNate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
-                startActivity(mIntent);
+        btnAdd.setOnClickListener() {
+        num1 = editNum1.text.toString();
+        num2 = editNum2.text.toString();
 
-            }
-        });
+        if (num1.trim() == "" || num2.trim() == "") {
+        Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+        result = num1.toDouble() + num2.toDouble();
+        tvResult.text = "계산 결과 : " + result.toString();
+        }
+        }
+        // 빼기
+        btnSub.setOnClickListener {
+        // 3-1, EditText의 값을 읽어 온다
+        num1 = editNum1.text.toString()
+        num2 = editNum2.text.toString()
 
+        if (num1.trim() == "" || num2.trim() == "") {
+        Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+        // 3-2, 계산
+        result = num1.toDouble() - num2.toDouble()
+        // 3-3, 계산된 결과를 TextView에 보여준다
+        tvResult.text = "계산 결과 : " + result.toString()
+        }
+        }
+        // 곱하기
+        btnMul.setOnClickListener {
+        // 3-1, EditText의 값을 읽어 온다
+        num1 = editNum1.text.toString()
+        num2 = editNum2.text.toString()
 
-        btn911.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
-                startActivity(mIntent);
-            }
-        });
+        if (num1.trim() == "" || num2.trim() == "") {
+        Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+        // 3-2, 계산
+        result = num1.toDouble() * num2.toDouble()
+        // 3-3, 계산된 결과를 TextView에 보여준다
+        tvResult.text = "계산 결과 : " + result.toString()
+        }
+        }
+        // 나누기
+        btnDiv.setOnClickListener{
+        // 3-1, EditText의 값을 읽어 온다
+        num1=editNum1.text.toString()
+        num2=editNum2.text.toString()
 
-        btnGal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("content://media/internal/images/media"));
-                startActivity(mIntent);
-            }
-        });
+        if(num1.trim()==""||num2.trim()==""){
+        Toast.makeText(applicationContext,"입력 값이 비었습니다.",Toast.LENGTH_SHORT).show()
+        }else{
+        if(num2.trim()=="0"){
+        Toast.makeText(applicationContext,"0으로 나머지 계산 불가합니다.",Toast.LENGTH_SHORT).show()
+        }else{
+        // 3-2, 계산
+        result=num1.toDouble()/num2.toDouble()
+        // 3-3, 계산된 결과를 TextView에 보여준다
+        tvResult.text="계산 결과 : "+result.toString()
+        }
+        }
+        }
+        btnRest.setOnClickListener {
+        // 3-1, EditText의 값을 읽어 온다
+        num1 = editNum1.text.toString()
+        num2 = editNum2.text.toString()
 
-        btnEnd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-
-            }
-        });
-
-    }
-
-}
+        if (num1.trim() == "" || num2.trim() == "") {
+        Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+        if (num2.trim() == "0") {
+        Toast.makeText(applicationContext, "0으로 나머지 계산 불가합니다.", Toast.LENGTH_SHORT).show()
+        } else {
+        // 3-2, 계산
+        result = num1.toDouble() % num2.toDouble()
+        // 3-3, 계산된 결과를 TextView에 보여준다
+        tvResult.text = "계산 결과 : " + result.toString()
+        }
+        }
+        }
+        }
+        }
