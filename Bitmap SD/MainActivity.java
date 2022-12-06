ae034f3c82639e2496e45c3090fd79e1ae92a4da
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-12-06 16:11:48 +0900

    P.366 Bitmap SD

---
+++ Bitmap SD/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cookandroid.bitmapsd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyGraphicView(this));
+        setTitle("비트맵 예제(SD카드)");
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+    }
+
+    private static class MyGraphicView extends View {
+        public MyGraphicView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            Bitmap picture = BitmapFactory.decodeFile("sdcard/Pictures/bee.png");
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
+            canvas.drawBitmap(picture, picX, picY, null);
+            picture.recycle();
+        }
+    }
+}
